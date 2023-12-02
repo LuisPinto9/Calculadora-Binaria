6e9fc1685ecb9a7064ebf2635efca662565e7bb7
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2023-12-02 15:04:33 -0500

    Arreglo de desplazamientos

--- Script.js
+++ Script.js
@@ -6,9 +6,9 @@ function changeBits() {
   bits = document.getElementById("bits-options").value;
 }
 
-function complementoDos(decimal, bits) {
+function complementoDos(decimal) {
   if (decimal >= 0) {
-    return decimalToBinary(decimal, bits); // Si es positivo, devuelve la representación binaria normal
+    return decimalToBinary(decimal); // Si es positivo, devuelve la representación binaria normal
   } else {
     let positiveBinary = decimalToBinary(Math.abs(decimal), bits); // Representación binaria positiva
     let invertedBinary = positiveBinary
@@ -29,7 +29,7 @@ function complementoDos(decimal, bits) {
   }
 }
 
-function decimalToBinary(decimal, bits) {
+function decimalToBinary(decimal) {
   let binary = (decimal >>> 0).toString(2); // Convierte a binario sin signo
 
   // Añade ceros a la izquierda para cumplir con el número de bits especificado
@@ -40,11 +40,19 @@ function decimalToBinary(decimal, bits) {
   return binary;
 }
 
-function binaryToDecimal(binary) {
-  return parseInt(binary, 2);
+function complementoDosToDecimal(binary) {
+  return binary
+    .split("")
+    .reverse()
+    .map((bit, index) =>
+      binary.length - 1 == index
+        ? Math.pow(2, index) * bit * -1
+        : Math.pow(2, index) * bit
+    )
+    .reduce((a, b) => a + b, 0);
 }
 
-function sumBinary(num1, num2, bits) {
+function sumBinary(num1, num2) {
   // Convierte los números binarios a decimales
   const decimal1 = complementoDosToDecimal(num1);
   const decimal2 = complementoDosToDecimal(num2);
@@ -58,14 +66,19 @@ function sumBinary(num1, num2, bits) {
 
   // Si hay sobreflujo, ajusta el valor
   if (sum > maxDecimalValue || sum < minDecimalValue) {
+    alert(
+      `El resultado de la suma se sale del rango de números representables [${minDecimalValue}, ${maxDecimalValue}], intente utilizar ${
+        bits * 2
+      } bits`
+    );
     sum = sum & ((1 << bits) - 1); // Aplica máscara para ajustar al rango permitido
   }
 
   // Convierte el resultado a binario en complemento a 2
-  return complementoDos(sum, bits);
+  return complementoDos(sum);
 }
 
-function subtractBinary(num1, num2, bits) {
+function subtractBinary(num1, num2) {
   // Convierte los números binarios a decimales
   const decimal1 = complementoDosToDecimal(num1);
   const decimal2 = complementoDosToDecimal(num2);
@@ -79,14 +92,19 @@ function subtractBinary(num1, num2, bits) {
 
   // Si hay sobreflujo o subflujo, ajusta el valor
   if (difference > maxDecimalValue || difference < minDecimalValue) {
+    alert(
+      `El resultado de la resta se sale del rango de números representables [${minDecimalValue}, ${maxDecimalValue}], intente utilizar ${
+        bits * 2
+      } bits`
+    );
     difference = difference & ((1 << bits) - 1); // Aplica máscara para ajustar al rango permitido
   }
 
   // Convierte el resultado a binario en complemento a 2
-  return complementoDos(difference, bits);
+  return complementoDos(difference);
 }
 
-function multiplyBinary(num1, num2, bits) {
+function multiplyBinary(num1, num2) {
   // Convierte los números binarios a decimales
   const decimal1 = complementoDosToDecimal(num1);
   const decimal2 = complementoDosToDecimal(num2);
@@ -96,30 +114,27 @@ function multiplyBinary(num1, num2, bits) {
 
   // Verifica si hay sobreflujo o subflujo
   const maxDecimalValue = Math.pow(2, bits - 1) - 1; // Calcula el máximo valor positivo representable
-  console.log(maxDecimalValue);
   const minDecimalValue = -Math.pow(2, bits - 1); // Calcula el máximo valor negativo representable
-  console.log(minDecimalValue);
 
   // Si hay sobreflujo o subflujo, ajusta el valor
   if (product > maxDecimalValue || product < minDecimalValue) {
     alert(
-      `El resultado de la multiplicación se sale del rango de números repressentables [${minDecimalValue}, ${maxDecimalValue}], intente utilizar ${bits * 2
+      `El resultado de la multiplicación se sale del rango de números representables [${minDecimalValue}, ${maxDecimalValue}], intente utilizar ${
+        bits * 2
       } bits`
     );
-    product = "0";
-    // product = product & ((1 << bits) - 1); // Aplica máscara para ajustar al rango permitido
+    // product = "0";
+    product = product & ((1 << bits) - 1); // Aplica máscara para ajustar al rango permitido
   }
 
   // Convierte el resultado a binario en complemento a 2
-  return complementoDos(product, bits);
+  return complementoDos(product);
 }
 
-function divideBinary(num1, num2, bits) {
+function divideBinary(num1, num2) {
   // Convierte los números binarios a decimales
   const decimal1 = complementoDosToDecimal(num1);
-  console.log(decimal1);
   const decimal2 = complementoDosToDecimal(num2);
-  console.log(decimal2);
 
   // Manejo de división por cero
   if (decimal2 === 0) {
@@ -140,155 +155,72 @@ function divideBinary(num1, num2, bits) {
     remainder > maxDecimalValue ||
     remainder < minDecimalValue
   ) {
+    alert(
+      `El resultado de la división se sale del rango de números representables [${minDecimalValue}, ${maxDecimalValue}], intente utilizar ${
+        bits * 2
+      } bits`
+    );
     quotient = quotient & ((1 << bits) - 1); // Aplica máscara para ajustar el cociente al rango permitido
     remainder = remainder & ((1 << bits) - 1); // Aplica máscara para ajustar el residuo al rango permitido
   }
 
   // Convierte el cociente y el residuo a binario en complemento a 2
-  const quotientBinary = complementoDos(quotient, bits);
-  const remainderBinary = complementoDos(remainder, bits);
+  const quotientBinary = complementoDos(quotient);
+  const remainderBinary = complementoDos(remainder);
 
   return { quotient: quotientBinary, remainder: remainderBinary };
 }
 
-function leftShiftBinary(num, bits) {
-  // Convierte el número binario a decimal
-  const decimal = complementoDosToDecimal(num);
-
-  // Realiza el desplazamiento a la izquierda
-  let shifted = decimal << 1;
-
-  // Verifica si hay sobreflujo o subflujo
-  const maxDecimalValue = Math.pow(2, bits - 1) - 1; // Calcula el máximo valor positivo representable
-  const minDecimalValue = -Math.pow(2, bits - 1); // Calcula el máximo valor negativo representable
-
-  // Si hay sobreflujo o subflujo, ajusta el valor
-  if (shifted > maxDecimalValue || shifted < minDecimalValue) {
-    shifted = shifted & ((1 << bits) - 1); // Aplica máscara para ajustar al rango permitido
-  }
-
-  // Convierte el resultado a binario en complemento a 2
-  return complementoDos(shifted, bits);
+function enableCalculate() {
+  document.getElementById("btn-calculate").disabled = false;
 }
-
-function rightShiftBinary(num, bits) {
-  // Convierte el número binario a decimal
-  const decimal = complementoDosToDecimal(num);
-
-  // Realiza el desplazamiento a la derecha
-  let shifted = decimal >> 1;
-
-  // Verifica si el número original es negativo para replicar el bit de signo
-  if (decimal < 0) {
-    shifted |= 1 << (bits - 1); // Replica el bit de signo (1) a la derecha
-  }
-
-  // Convierte el resultado a binario en complemento a 2
-  return complementoDos(shifted, bits);
-}
-
-
-
-// function calculate() {
-//   const num1 = document.getElementById("num1").value;
-//   const num2 = document.getElementById("num2").value;
-//   const operator = document.getElementById("operator").value;
-//   const bits = parseInt(document.getElementById("bits").value); // Número de bits seleccionados
-
-//   let result;
-
-//   switch (operator) {
-//     case "add":
-//       result = sumBinary(num1, num2, bits);
-//       break;
-//     case "subtract":
-//       result = subtractBinary(num1, num2, bits);
-//       break;
-//     case "multiply":
-//       result = multiplyBinary(num1, num2, bits);
-//       break;
-//     case "divide":
-//       result = divideBinary(num1, num2, bits);
-//       break;
-//     case "leftShift":
-//       result = leftShiftBinary(num1, bits);
-//       break;
-//     case "rightShift":
-//       result = rightShiftBinary(num1, bits);
-//       break;
-//     default:
-//       break;
-//   }
-
-//   if (typeof result === "object") {
-//     document.getElementById(
-//       "result"
-//     ).value = `Cociente: ${result.quotient}, Residuo: ${result.remainder}`;
-//     document.getElementById(
-//       "result-decimal"
-//     ).value = `Cociente: ${complementoDosToDecimal(
-//       result.quotient
-//     )}, Residuo: ${complementoDosToDecimal(result.remainder)}`;
-//   } else {
-//     // result = "0" + result
-//     document.getElementById("result").value = result;
-//     document.getElementById("result-decimal").value =
-//       complementoDosToDecimal(result);
-//   }
-// }
 
 function calculate() {
   const numExpression = document.getElementById("num").textContent;
-  const bits = parseInt(document.getElementById("bits-options").value); 
-
 
   let operator = "";
   for (let i = 0; i < operators.length; i++) {
     if (numExpression.includes(operators[i])) {
       operator = operators[i];
-      break; 
+      break;
     }
   }
 
   if (operator) {
-    
     const [num1, num2] = numExpression.split(operator);
-    console.log("result", num1, num2, bits);
 
     let result;
 
     switch (operator) {
       case "+":
-        result = sumBinary(num1, num2, bits);
-        console.log("resultsss=",result);
+        result = sumBinary(num1, num2);
         break;
-      case "-":
-        result = subtractBinary(num1, num2, bits);
+      case "—":
+        result = subtractBinary(num1, num2);
         break;
       case "*":
-        result = multiplyBinary(num1, num2, bits);
+        result = multiplyBinary(num1, num2);
         break;
       case "/":
-        result = divideBinary(num1, num2, bits);
-        break;
-      case "leftShift":
-        result = leftShiftBinary(num1, bits);
-        break;
-      case "rightShift":
-        result = rightShiftBinary(num1, bits);
+        result = divideBinary(num1, num2);
         break;
       default:
         break;
     }
-
     if (typeof result === "object") {
-      document.getElementById("numResult").textContent = `Cociente: ${result.quotient}, Residuo: ${result.remainder}`;
-     document.getElementById("num-decimalResult").textContent = `Cociente: ${complementoDosToDecimal(result.quotient)}, Residuo: ${complementoDosToDecimal(result.remainder)}`;
+      document.getElementById(
+        "numResult"
+      ).textContent = `= Cociente: ${result.quotient}, Residuo: ${result.remainder}`;
+      document.getElementById(
+        "num-decimalResult"
+      ).textContent = `= Cociente: ${complementoDosToDecimal(
+        result.quotient
+      )}, Residuo: ${complementoDosToDecimal(result.remainder)}`;
     } else {
-      document.getElementById("numResult").textContent = result;
-       document.getElementById("num-decimalResult").textContent = complementoDosToDecimal(result);
+      document.getElementById("numResult").textContent = "= " + result;
+      document.getElementById("num-decimalResult").textContent =
+        "= " + complementoDosToDecimal(result);
     }
-
   } else {
     console.error("No se encontró un operador en la expresión.");
   }
@@ -308,11 +240,16 @@ function leftShift() {
   // Verificar que no haya otros operadores
   if (!hasOperators(num)) {
     // Desplazar a la izquierda
-    let shiftedNum = leftShiftBinary(num, 1);
+    let shiftedNum = leftShiftBinary(num);
     document.getElementById("num").textContent = shiftedNum;
     // Actualizar el valor decimal
-    document.getElementById("num-decimal").textContent = binaryToDecimal(shiftedNum);
+    document.getElementById("num-decimal").textContent =
+      complementoDosToDecimal(shiftedNum);
   }
+}
+
+function leftShiftBinary(num) {
+  return (result = num.substring(1) + "0");
 }
 
 // Función para realizar el desplazamiento a la derecha
@@ -324,67 +261,23 @@ function rightShift() {
     let shiftedNum = rightShiftBinary(num, 1);
     document.getElementById("num").textContent = shiftedNum;
     // Actualizar el valor decimal
-    document.getElementById("num-decimal").textContent = binaryToDecimal(shiftedNum);
+    document.getElementById("num-decimal").textContent =
+      complementoDosToDecimal(shiftedNum);
   }
 }
 
-
-
-function convertToDecimal(inputId) {
-  const binaryValue = document.getElementById(inputId).value;
-  const decimalValue = complementoDosToDecimal(binaryValue);
-  document.getElementById(inputId + "-decimal").value = decimalValue;
-}
-
-function complementoDosToDecimal(binary) {
-  // binary = "0" + binary
-  return binary
-    .split("")
-    .reverse()
-    .map((bit, index) =>
-      binary.length - 1 == index
-        ? Math.pow(2, index) * bit * -1
-        : Math.pow(2, index) * bit
-    )
-    .reduce((a, b) => a + b, 0);
-}
-
-function shiftsNum1() {
-  const num1 = document.getElementById("num1").value;
-  const operator = document.getElementById("operator").value;
-  const bits = parseInt(document.getElementById("bits").value); // Número de bits seleccionados
-  let result;
-
-  switch (operator) {
-    case "leftShift":
-      result = leftShiftBinary(num1, bits);
-      break;
-    case "rightShift":
-      result = rightShiftBinary(num1, bits);
-      break;
-    default:
-      break;
+function rightShiftBinary(num) {
+  const signBit = num.charAt(0);
+  let shifted = "0" + num.substring(0, num.length - 1);
+  if (signBit === "1") {
+    shifted = "1" + shifted.substring(1);
   }
-  document.getElementById("num1").value = result;
+  return shifted;
 }
 
-function shiftsNum2() {
-  const num2 = document.getElementById("num2").value;
-  const operator = document.getElementById("operator").value;
-  const bits = parseInt(document.getElementById("bits").value); // Número de bits seleccionados
-  let result;
-
-  switch (operator) {
-    case "leftShift":
-      result = leftShiftBinary(num2, bits);
-      break;
-    case "rightShift":
-      result = rightShiftBinary(num2, bits);
-      break;
-    default:
-      break;
-  }
-  document.getElementById("num2").value = result;
+function disableShift() {
+  document.getElementById("btn-<").disabled = true;
+  document.getElementById("btn->").disabled = true;
 }
 
 function appendToNum(digit) {
@@ -402,35 +295,53 @@ function appendToNum(digit) {
     }
   }
 
-  if (operatorCount < 1 || (operatorCount === 1 && !operators.includes(digit))) {
+  if (
+    operatorCount < 1 ||
+    (operatorCount === 1 && !operators.includes(digit))
+  ) {
     if (mode) {
       if (operatorCount == 1) {
         document.getElementById("num").textContent = num + digit;
         let num1 = num + digit;
-        const decimalValues = num1.split(operator).map((value) => complementoDosToDecimal(value)).join(operator);
+        const decimalValues = num1
+          .split(operator)
+          .map((value) => complementoDosToDecimal(value))
+          .join(operator);
         document.getElementById("num-decimal").textContent = decimalValues;
       } else {
         document.getElementById("num").textContent = num + digit;
         let decimalValues = "";
-        operators.includes(digit) ? (decimalValues = numDecimal + digit) : (decimalValues = complementoDosToDecimal(num + digit));
+        operators.includes(digit)
+          ? (decimalValues = numDecimal + digit)
+          : (decimalValues = complementoDosToDecimal(num + digit));
         document.getElementById("num-decimal").textContent = decimalValues;
       }
     } else {
       if (operatorCount == 1) {
         document.getElementById("num-decimal").textContent = numDecimal + digit;
         let numDecimal1 = numDecimal + digit;
-        const binary = numDecimal1.split(operator).map((value) => complementoDos(value, bits)).join(operator);
+        const binary = numDecimal1
+          .split(operator)
+          .map((value) => complementoDos(value, bits))
+          .join(operator);
         document.getElementById("num").textContent = binary;
       } else {
         document.getElementById("num-decimal").textContent = numDecimal + digit;
         let binary = "";
-        operators.includes(digit) ? (binary = num + digit) : (binary = complementoDos(numDecimal + digit, bits));
+        operators.includes(digit)
+          ? (binary = num + digit)
+          : (binary = complementoDos(numDecimal + digit, bits));
         document.getElementById("num").textContent = binary;
       }
     }
   } else {
     alert("No se puede ingresar más de un operador.");
   }
+}
+
+function clearResult() {
+  document.getElementById("numResult").textContent = ""
+  document.getElementById("num-decimalResult").textContent = ""
 }
 
 function clearEntry() {
@@ -453,8 +364,16 @@ function clearEntry() {
           operatorCount++;
         }
       }
+      if (operatorCount == 0) {
+        document.getElementById("btn-<").disabled = false;
+        document.getElementById("btn->").disabled = false;
+        document.getElementById("btn-calculate").disabled = true;
+      }
       if (operatorCount == 1) {
-        const decimalValues = num.split(operator).map((value) => complementoDosToDecimal(value)).join(operator);
+        const decimalValues = num
+          .split(operator)
+          .map((value) => complementoDosToDecimal(value))
+          .join(operator);
         document.getElementById("num-decimal").textContent = decimalValues;
       } else {
         const decimalValues = complementoDosToDecimal(num);
@@ -476,8 +395,16 @@ function clearEntry() {
           operatorCount1++;
         }
       }
+      if (operatorCount1 == 0) {
+        document.getElementById("btn-<").disabled = false;
+        document.getElementById("btn->").disabled = false;
+        document.getElementById("btn-calculate").disabled = true;
+      }
       if (operatorCount1 == 1) {
-        const binary = numDecimal.split(operator1).map((value) => complementoDos(value, bits)).join(operator1);
+        const binary = numDecimal
+          .split(operator1)
+          .map((value) => complementoDos(value, bits))
+          .join(operator1);
         document.getElementById("num").textContent = binary;
       } else {
         const binary = complementoDos(numDecimal, bits);
